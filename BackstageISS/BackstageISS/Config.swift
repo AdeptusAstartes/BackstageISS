47137877d8716c91ebc96e741d8ab09473bee2be
Donald Angelillo <dangelillo@gmail.com>
2021-12-13 23:27:08 -0500

    create app config struct

---
+++ BackstageISS/BackstageISS/Config.swift
@@ -0,0 +1,16 @@
+//
+//  Config.swift
+//  BackstageISS
+//
+//  Created by Donald Angelillo on 12/13/21.
+//
+
+import Foundation
+
+struct Config {
+    static let baseAPIURL = "http://api.open-notify.org"
+    
+    // Only using force unwrapped optionals here because there is zero chance of failure
+    static let issCurrentLocationAPIURL = URL(string: Config.baseAPIURL + "/iss-now/")!
+    static let issPassAPIURL = URL(string: Config.baseAPIURL + "/iss/v1/")!
+}
