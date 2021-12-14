2b510e061441f248d549cb711f2552cb571a79d4
Donald Angelillo <dangelillo@gmail.com>
2021-12-14 02:25:42 -0500

    Colors struct

---
+++ BackstageISS/BackstageISS/Utils/Colors.swift
@@ -0,0 +1,13 @@
+//
+//  Colors.swift
+//  BackstageISS
+//
+//  Created by Donald Angelillo on 12/14/21.
+//
+
+import UIKit
+
+struct Colors {
+    static let nasaBlue = #colorLiteral(red: 0.0431372549, green: 0.2392156863, blue: 0.568627451, alpha: 1)
+    static let nasaRed = #colorLiteral(red: 0.9882352941, green: 0.2392156863, blue: 0.1294117647, alpha: 1)
+}
